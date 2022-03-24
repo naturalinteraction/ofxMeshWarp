646581cdc68b0c9e4de24ed5f6cd585763d9e2a8
naturalinteraction <av@naturalinteraction.org>
2022-03-24 09:32:17 +0100

    game mode test

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -10,10 +10,40 @@ int main( ){
 	// pass in width and height too:
 	// ofRunApp(new ofApp());
 
-
     ofGLFWWindowSettings settings;
-    settings.windowMode = OF_FULLSCREEN;
-    settings.multiMonitorFullScreen = true;
-    ofCreateWindow(settings);
+    settings.windowMode = OF_GAME_MODE;
+    // settings.multiMonitorFullScreen = true;
+
+    settings.setSize(1920, 1080);
+    //settings.decorated = false;
+    settings.setPosition(ofVec2f(1920,0));
+    //settings.resizable = true;
+    settings.monitor = 1; // Specify monitor one
+    ofCreateWindow(settings) -> setVerticalSync(false);
+    ofAppGLFWWindow::listMonitors();  // prints to console
     ofRunApp(new ofApp);
+
+    /*
+    shared_ptr<ofAppBaseWindow> mainWindow = ofCreateWindow(settings);
+
+    settings.setSize(300, 300);
+    settings.setPosition(ofVec2f(0,0));
+    settings.resizable = false;
+    settings.monitor = 1; // Specify monitor two
+    shared_ptr<ofAppBaseWindow> guiWindow = ofCreateWindow(settings);
+    guiWindow->setVerticalSync(false);
+    shared_ptr<ofApp> mainApp(new ofApp);
+
+    ofRunApp(mainWindow, mainApp);
+    ofRunMainLoop();
+
+    per fare andare quanto sopra:
+
+    settings.windowMode = ofWindowMode::OF_GAME_MODE
+
+    oppure (funziona su windows):
+
+    settings.decorated = false
+    settings.setPosition(glm::vec2(1920.f, 0 ));
+    */
 }

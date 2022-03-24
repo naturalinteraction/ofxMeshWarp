ee763f1a9bbadc3450bab12c8fb25f0aa741f3cc
naturalinteraction <av@naturalinteraction.org>
2022-03-24 10:48:14 +0100

    test with fullscreen mode

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -11,44 +11,31 @@ int main( ){
 	// ofRunApp(new ofApp());
 
     ofGLFWWindowSettings settings;
-    settings.windowMode = OF_GAME_MODE;
-    settings.multiMonitorFullScreen = true;
+    settings.windowMode = OF_FULLSCREEN;
+    settings.multiMonitorFullScreen = false;
 
+    settings.resizable = true;
     settings.setSize(1920, 1080);
-    //settings.decorated = false;
-    settings.setPosition(ofVec2f(1920,0));
-    //settings.resizable = true;
-    settings.monitor = 1; // Specify monitor
+    settings.decorated = false;
+    settings.setPosition(ofVec2f(0,0));
+    settings.monitor = 0; // Specify monitor
     shared_ptr<ofAppBaseWindow> window1 = ofCreateWindow(settings);
     window1 -> setVerticalSync(false);
-    ofAppGLFWWindow::listMonitors();  // prints to console
+    //ofAppGLFWWindow::listMonitors();  // prints to console
     shared_ptr<ofApp> app1(new ofApp());
     ofRunApp(window1, app1);
 
-    settings.setPosition(ofVec2f(1920,0));
-    settings.monitor = 0; // Specify monitor
+    settings.setPosition(ofVec2f(0,0));
+    settings.monitor = 1; // Specify monitor
     shared_ptr<ofAppBaseWindow> window0 = ofCreateWindow(settings);
     window0 -> setVerticalSync(false);
-    ofAppGLFWWindow::listMonitors();  // prints to console
+    //ofAppGLFWWindow::listMonitors();  // prints to console
     shared_ptr<ofApp> app0(new ofApp());
     ofRunApp(window0, app0);
 
     ofRunMainLoop();
 
     /*
-    shared_ptr<ofAppBaseWindow> mainWindow = ofCreateWindow(settings);
-
-    settings.setSize(300, 300);
-    settings.setPosition(ofVec2f(0,0));
-    settings.resizable = false;
-    settings.monitor = 1; // Specify monitor two
-    shared_ptr<ofAppBaseWindow> guiWindow = ofCreateWindow(settings);
-    guiWindow->setVerticalSync(false);
-    shared_ptr<ofApp> mainApp(new ofApp);
-
-    ofRunApp(mainWindow, mainApp);
-    ofRunMainLoop();
-
     per fare andare quanto sopra:
 
     settings.windowMode = ofWindowMode::OF_GAME_MODE

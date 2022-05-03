cadf617945c9702e4ac76b70474f4d8144a3048c
naturalinteraction <av@naturalinteraction.org>
2022-05-03 12:47:11 +0200

    fixed distribute.sh

--- example/distribute.sh
+++ example/distribute.sh
@@ -1,5 +1,4 @@
 make -j8
 
 zip "pollex.zip" -re bin/example
-# install*.sh
-aws s3 mv "pollex.zip" s3://natural-interaction/files/
+aws s3 mv "pollex.zip" s3://natural-interaction/files/development/

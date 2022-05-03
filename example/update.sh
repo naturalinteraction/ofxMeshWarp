20843b5c0677924703fc4a655de599706683868b
av <av@naturalinteraction.org>
2022-05-03 12:28:13 +0200

    fixed update.sh

--- example/update.sh
+++ example/update.sh
@@ -1,4 +1,5 @@
-wget http://natural-interaction.s3-eu-west-1.amazonaws.com/files/pollex.zip
+# wget http://natural-interaction.s3-eu-west-1.amazonaws.com/files/development/poll_environment.zip
+wget http://natural-interaction.s3-eu-west-1.amazonaws.com/files/development/pollex.zip
 unzip pollex.zip
 rm pollex.zip
 

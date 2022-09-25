65e9a1ff1d886057bc5413157f7efed8a1d36f7b
naturalinteraction <av@naturalinteraction.org>
2022-09-25 15:28:55 +0200

    install missing libraries on Pollino machine (22.04)

--- example/install_other.sh
+++ example/install_other.sh
@@ -1,4 +1,5 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install -y libglfw3 net-tools glmark2 evtest sysstat htop wget
-# mesa-utils
+sudo apt-get install -y libglfw3 net-tools glmark2 evtest sysstat htop wget libfreeimage-dev libglew-dev liburiparser-dev
+
+sudo cp libboost_filesystem.so.1.74.0 /lib/x86_64-linux-gnu/

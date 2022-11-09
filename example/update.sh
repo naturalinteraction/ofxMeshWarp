df9210dd4b87b7a2f262ddfa3b56b9514d89c8ab
naturalinteraction <av@naturalinteraction.org>
2022-11-09 08:39:04 +0100

    update without prompting

--- example/update.sh
+++ example/update.sh
@@ -1,5 +1,4 @@
-# wget http://natural-interaction.s3-eu-west-1.amazonaws.com/files/development/poll_environment.zip
 wget http://natural-interaction.s3-eu-west-1.amazonaws.com/files/development/pollex.zip
-unzip pollex.zip
+unzip -o pollex.zip
 rm pollex.zip
 

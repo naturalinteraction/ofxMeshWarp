901d80ca7730497d35bbe89465cb7d0ea42d2f74
naturalinteraction <av@naturalinteraction.org>
2022-11-09 08:30:20 +0100

    plastico

--- example/distribute.sh
+++ example/distribute.sh
@@ -1,4 +1,5 @@
 make -j8
 
-zip "pollex.zip" bin/example bin/data/gradient1.png bin/data/gradient1reversed.png
+cp bin/example bin/plastico
+zip "pollex.zip" bin/plastico bin/data/gradient1.png bin/data/gradient1reversed.png
 aws s3 mv "pollex.zip" s3://natural-interaction/files/development/

683d0c11086442c96356caa73aa516c5c7e2c960
naturalinteraction <av@naturalinteraction.org>
2022-11-09 08:44:54 +0100

    distribute istruzioni

--- example/distribute.sh
+++ example/distribute.sh
@@ -1,5 +1,5 @@
 make -j8
 
 cp bin/example bin/plastico
-zip "pollex.zip" update.sh bin/plastico bin/data/gradient1.png bin/data/gradient1reversed.png
+zip "pollex.zip" istruzioni.txt update.sh bin/plastico bin/data/gradient1.png bin/data/gradient1reversed.png
 aws s3 mv "pollex.zip" s3://natural-interaction/files/development/

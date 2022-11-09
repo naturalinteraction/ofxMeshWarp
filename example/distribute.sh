568d62d03a220c83b830b61861e475b483d4e602
naturalinteraction <av@naturalinteraction.org>
2022-11-09 08:14:17 +0100

    distribute.sh

--- example/distribute.sh
+++ example/distribute.sh
@@ -1,4 +1,4 @@
 make -j8
 
-zip "pollex.zip" bin/example
+zip "pollex.zip" bin/example bin/data/gradient1.png bin/data/gradient1reversed.png
 aws s3 mv "pollex.zip" s3://natural-interaction/files/development/

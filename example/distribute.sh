ee50dcd4c8c8e85c268784702f6a1c79369e198f
naturalinteraction <av@naturalinteraction.org>
2022-11-30 15:39:22 +0100

    do not distribute the unused masks

--- example/distribute.sh
+++ example/distribute.sh
@@ -1,5 +1,5 @@
 make -j8
 
 cp bin/example bin/plastico
-zip "pollex.zip" istruzioni.txt update.sh bin/plastico bin/data/gradient1.png bin/data/gradient1reversed.png bin/data/mask1.png bin/data/mask2.png
+zip "pollex.zip" istruzioni.txt update.sh bin/plastico bin/data/gradient1.png bin/data/gradient1reversed.png
 aws s3 mv "pollex.zip" s3://natural-interaction/files/development/
